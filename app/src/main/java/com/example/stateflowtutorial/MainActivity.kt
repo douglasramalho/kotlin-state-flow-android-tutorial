bf4c32125bc732fb248482ae4a000316c3b127ff
Douglas Motta <tibeca@gmail.com>
2021-02-07 14:22:48 +0000

    Adjustments on activity

--- app/src/main/java/com/example/stateflowtutorial/MainActivity.kt
+++ app/src/main/java/com/example/stateflowtutorial/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.stateflowtutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.stateflowtutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onStop() {
+        super.onStop()
     }
 }
